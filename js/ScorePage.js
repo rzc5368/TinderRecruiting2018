79b7c4ef2f61b084b884d6361960a67d1f60a014
Rodrigo <rzc5368@gmail.com>
2019-02-14 11:06:04 -0500

    Graph action listeners complete

--- js/ScorePage.js
+++ js/ScorePage.js
@@ -1,98 +1,47 @@
-//
-//function displayMajor() {
-//
-//    document.getElementById("match").innerHTML = localStorage.getmajor;
-//
-//};
-
-
-//function displayMajor() {
-//
-//    var chosenMajor =  localStorage.getmajor;
-//
-//    if(chosenMajor == "Data Sciences"){
-//        document.getElementById("match").innerHTML = localStorage.getmajor;
-//    }
-//    else if(chosenMajor == "Information Science & Technology"){
-//        document.getElementById("match").innerHTML = localStorage.getmajor;
-//
-//
-//    }
-//    else if(chosenMajor == "Security Risk Analysis"){
-//         document.getElementById("match").innerHTML = localStorage.getmajor;
-//    }
-//    else if(chosenMajor == "Cyber Security"){
-//          document.getElementById("match").innerHTML = localStorage.getmajor;
-//    }
-//
-//};
-
 function majorGraphCalculations() {
-
+//*** Card count of each major passed from index.js***
     var totalNumCards = localStorage.getcardTotals;;
     var countIST = localStorage.getcountIST;
     var countCS = localStorage.getcountCyber;
     var countDS = localStorage.getcountDS;
     var countSRA = localStorage.getcountSRA;
+//***End Card count of each major passed from index.js***
 
+//***Percentage of each major***
     var istBar = (countIST / totalNumCards) * 100;
     var csBar =  (countCS / totalNumCards) * 100;
     var dsBar =  (countDS / totalNumCards) * 100;
     var sraBar = (countSRA / totalNumCards) * 100;
+//***End Percentage of each major***
 
-    // ****Testing Purposes.***8
-    //    console.log(totalNumCards);
-    //    console.log(countDS);
-//        console.log(dsBar);
-//        console.log(csBar);
-//        console.log(istBar);
-//        console.log(sraBar);
-    // **** End Testing****
-
-    var currentMajorPage = document.getElementById("match").textContent;
-
+//*** First four variables store a string of the name of the class for each bar in the graph in each html score page.
+//The percentage number is added to the end of the string because there is a class for each percentage***
     var istClassName = "percentage percentage-" + Math.ceil(istBar);
     var csClassName = "percentage percentage-" + Math.ceil(csBar);
     var dsClassName = "percentage percentage-" + Math.ceil(dsBar);
     var sraClassName = "percentage percentage-" + Math.ceil(sraBar);
+//***End variables***
 
-    console.log("IST: " + istClassName);
-    console.log("CS: " + csClassName);
-    console.log("SRA: " + sraClassName);
-    console.log("DS: " + dsClassName);
+//***JQuery to add class. Class name variables will change the value of the graph on the x-axis***
+     $('#istBar').addClass(istClassName);
+     $('#sraBar').addClass(sraClassName);
+     $('#dsBar').addClass(dsClassName);
+     $('#csBar').addClass(csClassName);
+//***End JQuery***
 
-    // ****Testing Purposes.***
-
-//        $('#istBar').addClass(istClassName);
-//        $('#sraBar').addClass(sraClassName);
-//        $('#dsBar').addClass(dsClassName);
-//        $('#csBar').addClass(csClassName);
-    // **** End Testing****
-
-
-//    if(currentMajorPage == "Information Sciences & Technology"){
-//
-//       $('#istBar').addClass('percentage percentage-95');
-//       $('#dsBar').addClass('percentage percentage-1');
-//       $('#csBar').addClass('percentage percentage-1');
-//       $('#sraBar').addClass('percentage percentage-1');
-//
-//
-//    }
-//    if(currentMajorPage == "Data Science"){
-//
-//       $('#dsBar').addClass('percentage percentage-1');
-//    }
-//    if(currentMajorPage == "Cyber Security"){
-//
-//       $('#csBar').addClass('percentage percentage-1');
-//    }
-//    if(currentMajorPage == "Security Risk Analysis"){
-//
-//       $('#sraBar').addClass('percentage percentage-1');
-//    }
-
-
-
+//***Action Event Listeners. When bar is clicked it will redirect the user to that specific html page according to bar clicked that's associated with that major"
+     document.getElementById("istBar").addEventListener("click", function() {
+        window.location.href = '/TinderRecruiting2018/ScorePageIST.html' ;
+        });
+     document.getElementById("sraBar").addEventListener("click", function() {
+          window.location.href = '/TinderRecruiting2018/ScorePageSRA.html' ;
+        });
+     document.getElementById("dsBar").addEventListener("click", function() {
+          window.location.href = '/TinderRecruiting2018/ScorePage.html' ;
+        });
+     document.getElementById("csBar").addEventListener("click", function() {
+          window.location.href = '/TinderRecruiting2018/ScorePageCS.html' ;
+        });
+//***End action Listeners***
 }
 
